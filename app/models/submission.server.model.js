f83a8ba4e7ae54524e2192036d57eb478fa80274
Rebecca Pipkin <relooga@gmail.com>
2014-08-13 19:31:40 -0600

    Add submission schema

--- app/models/submission.server.model.js
+++ app/models/submission.server.model.js
@@ -10,20 +10,55 @@ var mongoose = require('mongoose'),
  * Submission Schema
  */
 var SubmissionSchema = new Schema({
+	//name, user, bio, and email can be pulled from the user information originally put in
 	name: {
 		type: String,
 		default: '',
 		required: 'Please fill Submission name',
 		trim: true
 	},
+    bio: {
+        type: String,
+        required: 'Tell us about yourself'
+    },
+    user: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    },
+    email: {
+        type: String,
+        required: 'Please put in your email'
+    },
 	created: {
 		type: Date,
 		default: Date.now
 	},
-	user: {
-		type: Schema.ObjectId,
-		ref: 'User'
-	}
+
+    projectTitle: {
+        type: String,
+        required: 'Please enter a project title'
+    },
+    neighborhood: {
+        type: String,
+        required: 'Please put the neighborhood that this happened in'
+    },
+    map: {
+        type: String,
+        required: 'Please put coordinates here'
+    },
+    zipCode: {
+        type: Number,
+        required: '4 digit zip codes only'
+    },
+    description: {
+        type: String,
+
+    },
+    // change this once we know how to have it store files
+    fileUpload: {
+        type: String
+    }
+
 });
 
 mongoose.model('Submission', SubmissionSchema);
