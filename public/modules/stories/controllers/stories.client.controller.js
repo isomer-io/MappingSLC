9a15cb14f1ab7de76cf8341b32b421d5dc2957cb
PoetsRock <christanseer@hotmail.com>
2014-08-20 20:02:27 -0600

    added facebook OAuth controller and login link on client.view.html

--- public/modules/stories/controllers/stories.client.controller.js
+++ public/modules/stories/controllers/stories.client.controller.js
@@ -61,5 +61,16 @@ angular.module('stories').controller('StoriesController', ['$scope', '$statePara
 				storyId: $stateParams.storyId
 			});
 		};
+
+
+        // Adds logic for Facebook OAuth button
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=319019724936363&version=v2.0";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
 	}
 ]);
