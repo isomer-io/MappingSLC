ec23202354d9e6bb888d58bed99dd8d55b96b6b8
PoetsRock <christanseer@hotmail.com>
2014-11-07 10:55:38 -0700

    more ignore

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,5 @@
 'use strict';
-//test
+//test1
 module.exports = {
 	db: 'mongodb://localhost/mapping-salt-lake-city-dev',
 	app: {
--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,5 @@
 'use strict';
-//test
+//test1
 module.exports = {
 	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/mapping-salt-lake-city',
 	assets: {
