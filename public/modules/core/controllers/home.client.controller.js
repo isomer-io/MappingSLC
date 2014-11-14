f92c629d59587b821b23545e4338095e9693f78b
PoetsRock <christanseer@hotmail.com>
2014-11-13 18:41:56 -0700

    edited layout

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,8 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
 		$scope.toggle = true;
+		//todo create a function that makes photo1 and 2 dynamic rather than hard-coded.
+		$scope.photo1 = "as";
+		$scope.photo2 = "dw";
 	}
 ]);
