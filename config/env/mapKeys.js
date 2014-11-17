c044be7c89a39d0e375210f811045cfa953a80ba
PoetsRock <christanseer@hotmail.com>
2014-11-17 12:58:53 -0700

    updated git ignore

--- config/env/mapKeys.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by poetsrock on 11/17/14.
- */
-module.exports = {
-
-    mapboxKey: 'poetsrock.map-55znsh8b',
-    mapboxAccessToken: 'pk.eyJ1IjoicG9ldHNyb2NrIiwiYSI6Imc1b245cjAifQ.vwb579x58Ma-CcnfQNamiw'
-
-};
