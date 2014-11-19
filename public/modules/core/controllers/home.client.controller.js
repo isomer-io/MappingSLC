ed46d0c014de5c304ab529c02b6ce42d58158616
PoetsRock <christanseer@hotmail.com>
2014-11-18 18:49:14 -0700

    cleaned up files

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -8,22 +8,6 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 		$scope.toggle = true;
 		$scope.sidemenu = false;
 
-
-		//var sideMenuOn = element(by.css('containerOn'));
-		//var sideMenuOff = element(by.css('containerOn'));
-        //
-		//it('should check ng-show / ng-hide', function() {
-		//	expect(sideMenuOn.isDisplayed()).toBeTruthy();
-		//	expect(sideMenuOff.isDisplayed()).toBeFalsy();
-        //
-		//$scope.element(by.model('clicked')).click();
-        //
-		//	expect(sideMenuOn.isDisplayed()).toBeFalsy();
-		//	expect(sideMenuOff.isDisplayed()).toBeTruthy();
-		//});
-
-
-
 		//placeholder for featured projects images
 		//todo once admin module is built, create a function that makes photo1 and 2 dynamic rather than hard-coded
 		$scope.photo1 = 'as';
