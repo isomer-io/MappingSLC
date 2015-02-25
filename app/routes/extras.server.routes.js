be3a44875fa2a89598960b85a7d94e189c5892bc
Photoleague <stevensaintjames@yahoo.com>
2015-02-24 18:49:37 -0700

    Sync before merge

--- app/routes/extras.server.routes.js
+++ app/routes/extras.server.routes.js
@@ -1,40 +1,51 @@
-/**
-* Created by poetsrock on 11/18/14.
-*/
-
 'use strict';
 
-module.exports = function(app) {
+module.exports = function (app) {
     var keys = require('../../config/env/keys.js'),
+        tractData = require ('../../config/env/utahTract.json'),
         request = require('request');
 
     // Maps Routes
     app.route('/keys')
-        .get(function(req, res) {
-                res.jsonp(keys);
-            });
-
-    app.route('/places')
-        .get(function(req, res) {
-            request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyBZ63pS3QFjYlXuaNwPUTvcYdM-SGRmeJ0&location=40.773,-111.902&radius=1000',
-                function (error, response, body) {
-                    res.jsonp(JSON.parse(body));
-                });
+        .get(function (req, res) {
+            res.jsonp(keys);
         });
 
+    // Maps Routes
+    app.route('/tractData')
+        .get(function (req, res) {
+            res.jsonp(tractData);
+        });
+
+    app.route('/places')
+        .get(function (req, res) {
+            //var pageToken = '';
+            var query = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyBZ63pS3QFjYlXuaNwPUTvcYdM-SGRmeJ0&location=40.773,-111.902&radius=1000';
+            request(query,
+                function (error, response, body) {
+                    body = JSON.parse(body);
+                    res.jsonp(body);
+                    var pageToken = body['next_page_token'];
 
 
+                    var secondQuery = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=' + pageToken + '&key=AIzaSyBZ63pS3QFjYlXuaNwPUTvcYdM-SGRmeJ0';
+                    console.log(secondQuery);
+                    request(secondQuery, function(error, response, body) {
+                        console.log(body);
+                    });
 
 
+                    //request('https://www.google.com',
+                    //    function (error, response, body) {
+                    //    //body = JSON.parse(body);
+                    //    //res.jsonp(body);
+                    //    console.log(body);
+                    //});
 
 
+                    //return pageToken;
+                });
 
-    //
-    //var projectObj = require('../models/project.server.model.js');
-    //
-    //// Maps Routes
-    //app.route('/projectObj')
-    //    .get(function(req, res) {
-    //        res.jsonp(projectObj);
-    //    });
+        });
+
 };
--- public/modules/map/controllers/map.client.controller.js
+++ public/modules/map/controllers/map.client.controller.js
@@ -1,14 +1,10 @@
 'use strict';
 
-angular.module('map').controller('MapController', ['$scope', 'Authentication', 'MapboxApiKeys', '$http','ErrorHandleService',
-    function ($scope, Authentication, MapboxApiKeys, $http, ErrorHandleService) {
+angular.module('map').controller('MapController', ['$scope', 'Authentication', 'MapboxApiKeys', '$http',
+    function ($scope, Authentication, MapboxApiKeys, $http) {
 
         $scope.markers = true;
         $scope.filters = true;
-
-
-
-
 
         MapboxApiKeys.getApi()
             .success(function (data) {
@@ -19,55 +15,148 @@ angular.module('map').controller('MapController', ['$scope', 'Authentication', '
                 alert('Failed to load Mapbox API key. Status: ' + status);
             });
 
-
         var mapFunction = function (key, accessToken) {
 
             //creates a Mapbox Map
             L.mapbox.accessToken = accessToken;
-            var map = L.mapbox.map('map', key)
+            var map = L.mapbox.map('map')
                 .setView([40.773, -111.902], 12);
 
-            L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-                maxZoom: 18,
-                id: key
-            })
-            .addTo(map);
+            ////centers on marker when popup icon is clicked
+            //// source: https://www.mapbox.com/mapbox.js/example/v1.0.0/centering-markers/
+            //map.featureLayer.on('click', function(e) {
+            //    map.panTo(e.layer.getLatLng());
+            //});
 
+            L.control.layers({
+                'Main Map': L.mapbox.tileLayer('poetsrock.map-55znsh8b').addTo(map),
+                '2nd Map': L.mapbox.tileLayer('poetsrock.map-55znsh8b')
+            }, {
+                'Tract Boundaries': L.mapbox.tileLayer('poetsrock.7c0b2f7a'),
+                'Other': L.mapbox.tileLayer('poetsrock.control-room')
+            }).addTo(map);
+
+
+            var tractData = $http.get('/tractData')
+                .success(function (tractGeoJson) {
+                    tractDataLayer(tractGeoJson);
+                    console.log(tractGeoJson);
+                })
+                .error(function (tractErrorData) {
+                    console.log(tractErrorData);
+                }
+            );
+
+            //var tractDataLayer = function(tractGeoJson){
+            //    L.mapbox.featureLayer(tractGeoJson)
+            //        .addTo(map);
+            //};
+
+            var tractDataLayer = function (tractGeoJson) {
+                L.geoJson(tractGeoJson, {
+                        style: function (feature) {
+                            return {
+                                //properties can be found here: http://leafletjs.com/reference.html#path
+                                stroke: true,
+                                weight: 1,  //stroke in pixels
+                                color: '#010101',
+                                fill: false,
+                                clickable: true,
+                                className: ''  //String that sets custom class name on an element
+                            };
+                        },
+                        onEachFeature: function (feature, layer) {
+                            layer.bindPopup(feature.properties.description);
+                        }
+
+                    }
+                )
+                    .addTo(map);
+
+            };
+
+            //L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+            //    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            //    maxZoom: 18,
+            //    id: key
+            //})
+            //    .addTo(map);
 
             $http.get('/places').success(function (mapData) {
-                console.log(mapData);
+                console.log('mapData', mapData);
                 var placeLength = mapData.results.length;
                 for (var place = 0; place < placeLength; place++) {
 
                     var mapLat = mapData.results[place].geometry.location.lat;
                     var mapLng = mapData.results[place].geometry.location.lng;
+                    var mapTitle = mapData.results[place].name;
+                    //console.log('mapTitle: ', mapTitle);
                     //mapSmbol is blah blah
-                    var mapSymbol = function() {
-                        if(1 === 1){
+                    var mapSymbol = function () {
+                        if (mapData.results[place].types[0] !== 'neighborhood' && mapData.results[place].types[0] !== 'stadium' && mapData.results[place].types[0] !== 'store' && mapData.results[place].types[0] !== 'church' && mapData.results[place].types[0] !== 'clothing_store' && mapData.results[place].types[0] !== 'university' && mapData.results[place].types[0] !== 'establishment') {
                             return mapData.results[place].types[0];
-                        }else if(statusError === 400){
-                            var typesLength = mapData.results[place].types.length;
-                            for (var markerType = 0; markerType < typesLength; markerType++)
-                                return mapData.results[place].types[markerType];
-                        }else{
+                            //}else if(statusError === 400){
+                            //    var typesLength = mapData.results[place].types.length;
+                            //    for (var markerType = 0; markerType < typesLength; markerType++)
+                            //        return mapData.results[place].types[markerType];
+                        } else {
                             return 'marker';
                         }
 
                     };
-                    ErrorHandleService.response();
-                    console.log(mapSymbol());
+                    //console.log('mapSymbol(): ', mapSymbol());
 
-                    L.marker([mapLat, mapLng], {
-                        icon: L.mapbox.marker.icon({
-                            'marker-size': 'large',
-                            'marker-symbol': mapSymbol(),
-                            'marker-color': '#00295A'
-                        })
-                    })
-                    .addTo(map);
+                    //properties for markers are gathered from the api docs of both Mapbox and Leaflet.
+                    //Mapbox:
+                    //Leafleft: http://leafletjs.com/reference.html#marker,
+                    //http://leafletjs.com/reference.html#icon
+                    var styledIcon = L.mapbox.marker.icon({
+                        'title': mapTitle,
+                        'marker-size': 'large',
+                        'marker-symbol': mapSymbol(),
+                        'marker-color': '#00295A',
+                        'riseOnHover': true,
+                        'riseOffset': 250,
+                        'opacity': 0.1,
+                        'clickable': true
+                    });
+                    L.marker([mapLat, mapLng], {icon: styledIcon})
+                        .addTo(map);
                 }
+
+                //L.marker([mapLat, mapLng]).bindPopup(
+                //    {icon: styledIcon}
+                //)
+                //    .addTo(map);
+
+
             });
+
+            //creates filter
+            var filters = document.getElementById('filters');
+            var checkboxes = document.getElementsByClassName('filter');
+
+            function change() {
+                // Find all checkboxes that are checked and build a list of their values
+                var on = [];
+                for (var i = 0; i < checkboxes.length; i++) {
+                    if (checkboxes[i].checked) on.push(checkboxes[i].value);
+                }
+                // The filter function takes a GeoJSON feature object
+                // and returns true to show it or false to hide it.
+                map.featureLayer.setFilter(function (f) {
+                    // check each marker's symbol to see if its value is in the list
+                    // of symbols that should be on, stored in the 'on' array
+                    return on.indexOf(f.properties['marker-symbol']) !== -1;
+                });
+                return false;
+            }
+
+            // When the form is touched, re-filter markers
+            filters.onchange = change;
+            // Initially filter the markers
+            change();
+
         };
     }
 ]);
