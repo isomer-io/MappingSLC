51b76fc0c59c1547438a6397154b9130d153ebcf
PoetsRock <christanseer@hotmail.com>
2015-03-02 15:36:30 -0700

    cleaned up code

--- public/modules/map/controllers/map.client.controller.js
+++ public/modules/map/controllers/map.client.controller.js
@@ -123,7 +123,6 @@ angular.module('map').controller('MapController', ['$scope', 'Authentication', '
             //Google Places API
             var googlePlacesMarker = null;
             var googlePlacesMarkerLayer = null;
-            var mapSymbol = {};
             var googlePlacesMarkerArray = [];
 
             var googlePlacesData = function() {
@@ -169,44 +168,5 @@ angular.module('map').controller('MapController', ['$scope', 'Authentication', '
             };
 
         };
-
-        //
-        //
-        ////get Census Data from Census API
-        //$scope.method = 'GET';
-        //$scope.url = 'http://api.census.gov/data/2010/sf1?get=P0010001&for=tract:*&in=state:49+county:035&key=4d396163ae90829a66916a08b3af462608c87316';
-        //
-        //
-        ////make the api call
-        //$http({method: $scope.method, url: $scope.url, cache: $templateCache}).
-        //    success(function (censusData, status, headers, config) {
-        //        $scope.status = status;
-        //        $scope.censusData = censusData;
-        //        var i;
-        //        for (i = 0; i < censusData.length; i++) {
-        //            console.log('census data: ', censusData[i]);
-        //        }
-        //        console.log(status);
-        //        console.log(headers);
-        //        console.log(headers.length);
-        //        console.log(config);
-        //
-        //    }).
-        //    error(function (censusDataError, status) {
-        //        $scope.censusData = censusDataError || 'Request failed';
-        //        $scope.status = status;
-        //    });
-
-
     }
 ]);
-
-
-//mapSymbol = function () {
-//    if (poiData.results[place].types[0] !== 'neighborhood' && poiData.results[place].types[0] !== 'stadium' && poiData.results[place].types[0] !== 'store' && poiData.results[place].types[0] !== 'church' && poiData.results[place].types[0] !== 'clothing_store' && poiData.results[place].types[0] !== 'university' && poiData.results[place].types[0] !== 'establishment') {
-//        return poiData.results[place].types[0];
-//    } else {
-//        return 'marker';
-//    }
-//
-//};
