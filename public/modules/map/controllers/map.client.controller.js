f504ca77a6c98b892e4e5d7ddebc70adc1616877
Photoleague <stevensaintjames@yahoo.com>
2015-03-04 20:15:36 -0700

    Added main page side bar

--- public/modules/map/controllers/map.client.controller.js
+++ public/modules/map/controllers/map.client.controller.js
@@ -48,6 +48,34 @@ angular.module('map').controller('MapController', ['$scope', 'Authentication', '
                 //'Tract Boundaries': L.mapbox.tileLayer('examples.bike-lanes'),
             }).addTo(map);
 
+            L.mapbox.featureLayer({
+                // this feature is in the GeoJSON format: see geojson.org
+                // for the full specification
+                type: 'Feature',
+                geometry: {
+                    type: 'Point',
+                    // coordinates here are in longitude, latitude order because
+                    // x, y is the standard for GeoJSON and many formats
+                    coordinates: [
+                        -111.902,
+                        40.773
+                    ]
+                },
+                properties: {
+                    title: 'Peregrine Espresso',
+                    description: '1718 14th St NW, Washington, DC',
+                    // one can customize markers by adding simplestyle properties
+                    // https://www.mapbox.com/guides/an-open-platform/#simplestyle
+                    'marker-size': 'large',
+                    'marker-color': '#BE9A6B',
+                    'marker-symbol': 'cafe'
+                }
+            })
+            .on('click', function() {
+                setStyle({});
+            })
+            .addTo(map);
+
             //get the json file on the backend (/config/env/) for the Census Tract Data
             var tractData = $http.get('/tractData')
                 .success(function (tractGeoJson) {
