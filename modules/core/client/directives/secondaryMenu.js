ab04a01502ac8ff628663a065b86503b09b03b06
PoetsRock <chris@christanseer.com>
2015-10-11 13:22:54 -0600

    fixed scope in secondary-menu directive so admin link shows

--- modules/core/client/directives/secondaryMenu.js
+++ modules/core/client/directives/secondaryMenu.js
@@ -1,28 +1,20 @@
 'use strict';
 
 angular.module('core').directive('secondaryMenuDirective', function() {
-    //$scope, $rootScope, $broadcast, $location
+    
     return {
 
         restrict: 'E',
         templateUrl: '/modules/core/client/directives/views/secondary-menu-directive.html',
-        link: function(scope, $rootScope, $broadcast, $location, Users, AdminAuthService) {
+
+        controller: function(AdminAuthService, $scope){
+              $scope.isAdmin = AdminAuthService;
+        },
+
+        link: function(scope) {
 
             scope.secondMenuOpened = false;
             scope.toggleSecondMenu = false;
-
-            //runs a query to return user ID for admin panel editing
-            AdminAuthService.isAdminCheck = function () {
-                scope.find = function () {
-                    scope.users = Users.query();
-                    console.log(Users.query());
-                    console.log(scope.users);
-                };
-
-                //if (Users.query.roles() === 'admin') {
-                //    return true;
-                //}
-            };
 
         }
     }
