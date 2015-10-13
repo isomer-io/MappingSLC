b7bb9c4dba1e5ed23c5296e75694b18ece7ccbf7
PoetsRock <chris@christanseer.com>
2015-10-13 14:58:49 -0600

    updated users client and package.json

---
+++ modules/users/client/controllers/admin/users.client.controller.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('users.admin').controller('UserController', ['$scope', '$state', 'Authentication', 'userResolve', '$stateParams', 'Users', 'UserData',
+  function ($scope, $state, Authentication, userResolve, $stateParams, Users, UserData) {
+    $scope.authentication = Authentication;
+      // Find a list of Users
+          $scope.find = function() {
+            $scope.users = Users.query($scope.query);
+          };
+
+      // Find existing User
+          $scope.findOne = function() {
+            $scope.user = UserData.getUser({
+              userId: $stateParams.userId
+            });
+            console.log('$scope.users: ', $scope.users);
+          };
+
+          $scope.init = function() {
+            $scope.find();
+          };
+
+  }
+]);
+
+
+
