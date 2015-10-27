6624552ae8ac81ae7004839ad27e640668aca344
The MAIN Steve Brown <steve.brown@thomasarts.com>
2015-10-27 00:23:51 -0600

    refactored to fix issues but new user save does not save

--- modules/users/server/controllers/messages.server.controller.js
+++ modules/users/server/controllers/messages.server.controller.js
@@ -9,7 +9,7 @@ var path = require('path'),
     User = mongoose.model('User'),
     _ = require('lodash');
 
- function _checkForExistingUser (currentUniqueId) {
+ function checkForExistingUser (currentUniqueId) {
     User.findOne({
         'email': currentUniqueId
     })
@@ -26,6 +26,7 @@ var path = require('path'),
             } else {
 
                 if (users._doc.email === currentUniqueId) {
+                    console.log('not a new email!!!');
                     return true;
                 }
             }
@@ -33,47 +34,72 @@ var path = require('path'),
 };
 //Create a user for the Newsletter
 exports.subscriber = function (req,res) {
-    if (_checkForExistingUser(req.body.email)) {
-
-        if (req.user.newsletter == false){
-            req.user.newsletter = true;
+    User.findOne({
+        'email': req.body.email
+    }).exec(function (err, user) {
+        if (err) {
+            console.log('houston, we got a problem');
+        } else {
+            if (user) {
+                if (user.newsletter == true){
+                    res.send('you are already signed up for the newsletter!, Thanks')
+                }else{
+                    user.newsletter = true;
+                    user.save(function (err) {
+                        if (err) {
+                            console.log('problem with saving: ', err);
+                        } else {
+                            res.send('user added to newsletter');
+                        }
+                    });
+                }
+            } else {
+                var newsletteruser = new User();
+                delete newsletteruser.roles;
+                newsletteruser.newsletter = true;
+                newsletteruser.email = req.body.email;
+                console.log(newsletteruser);
+                newsletteruser.save(function (err) {
+                    if (err) {
+                        console.log('new user could not save: ', user);
+                    } else {
+                        req.send('New user added to DATABASE!!!');
+                    }
+                })
+            }
         }
-        User.save(function(err){
-            if (err) {
-                console.log('Houston we got a problem: ', err);
-            }else {
-                res.json(req.user);
-            }
-        })
-        //profile.update(req,res);
-
-      //res.json(req.user);
-
-
-
-    }else {
-        //add defaults for other required fields
-        //assign email address to user name
-        var user = new User({username: req.body.email});
-        user.newsletter = true;
-        user.email = req.body.email;
-        delete req.body.roles;
-        console.log('new user: ' ,user);
-        user.save(function(err){
-            if (err){
-                console.log('i gotta an error: ', err);
-                return res.status(400).send({
-                    message: errorHandler.getErrorMessage(err)
-                });
-            }else {
-                res.json(user);
-            }
-        })
-        //res.json(user);
-        //authenticate.signup(req,res);
-    }
-    //var user = new User(req.body);
-    //user.email = req.body.email;
-
-
+    });
 };
+//    if (checkForExistingUser(req.body.email)) {
+//                console.log('old user');
+//        if (req.user.newsletter == false){
+//            req.user.newsletter = true;
+//            User.save(function(err){
+//                if (err) {
+//                    console.log('Houston we got a problem: ', err);
+//                }else {
+//                    res.json(req.user);
+//                }
+//            });
+//        }
+//
+//    }else {
+//        console.log('this is if its a new user');
+//        var user = new User({username: req.body.email});
+//        user.newsletter = true;
+//        user.email = req.body.email;
+//        delete req.body.roles;
+//        console.log('new user: ' ,user);
+//        user.save(function(err){
+//            if (err){
+//                console.log('i gotta an error: ', err);
+//                return res.status(400).send({
+//                    message: errorHandler.getErrorMessage(err)
+//                });
+//            }else {
+//                res.json(user);
+//            }
+//        });
+//
+//    }
+//};
