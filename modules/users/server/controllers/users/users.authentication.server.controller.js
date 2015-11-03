1a0b9dcc57e487817b73a4890dd21a7f0e2d245e
The MAIN Steve Brown <steve.brown@thomasarts.com>
2015-11-03 00:09:22 -0700

    more sign up mayhem

--- modules/users/server/controllers/users/users.authentication.server.controller.js
+++ modules/users/server/controllers/users/users.authentication.server.controller.js
@@ -33,7 +33,9 @@ exports.signup = function (req, res) {
   // Then save the user
   user.save(function (err) {
     if (err) {
+      console.log('heres an error: ', err);
       return res.status(400).send({
+
         message: errorHandler.getErrorMessage(err)
       });
     } else {
@@ -43,6 +45,7 @@ exports.signup = function (req, res) {
 
       req.login(user, function (err) {
         if (err) {
+
           res.status(400).send(err);
         } else {
           res.json(user);
